559689163a6e2f1a6881101541d1d6f5e83978d1
Sukhyun Baik <neonagx@gmail.com>
2016-11-18 16:23:05 -0800

    adds express session on package and app.js

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session')
 
+
+//load the env vars
+require('dotenv').load()
+
+//create the Express app
+var app = express();
+
+require('./config/database')
+require('./config/passport')
+
+//require routes
 var index = require('./routes/index');
 var users = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
