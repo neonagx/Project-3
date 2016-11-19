880889b7050d54ea3850291e8ca654c2e198e8b4
Sukhyun Baik <neonagx@gmail.com>
2016-11-19 12:17:52 -0800

    adds routes login, logout to users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router()
+var passport = require('passport')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/',
+    failureRedirect : '/'
+  }
+));
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
 });
 
-module.exports = router;
+module.exports = router
