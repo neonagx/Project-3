8dddf5c1a8c6f637b7851e181fb433a285d9d31b
Sukhyun Baik <neonagx@gmail.com>
2016-11-19 21:22:51 -0800

    routes are fixed to go to index.ejs with working authentication

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/users', users)
+app.use('/', users)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
