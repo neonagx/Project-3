c71b5dc51daa0c26bfcc6da51692ca481b26b83d
Gaye Lowenstein <glowen@hotmail.com>
2016-11-21 12:25:52 -0800

    working merged version

--- models/user.js
+++ models/user.js
@@ -1,20 +1,5 @@
 var mongoose = require('mongoose')
 
-<<<<<<< HEAD
-var UserSchema = new mongoose.Schema({
-  userName: String,
-  movies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
-})
-
-UserSchema.methods.sayHello = function(){
-  console.log("Hi " + userName + ", is this working?")
-}
-
-var User = mongoose.model('User', UserSchema)
-
-//make available to our other files
-module.exports = User
-=======
 var userSchema = new mongoose.Schema({
   name: String,
   email: String,
@@ -22,4 +7,3 @@ var userSchema = new mongoose.Schema({
 })
 
 module.exports = mongoose.model('User', userSchema)
->>>>>>> a98edb413a42529b5257041e5e646b7dca9e2591
