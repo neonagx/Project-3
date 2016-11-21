abc2dfcb2c10c44eb91e2fb415d9c9179f7444c1
Sukhyun Baik <neonagx@gmail.com>
2016-11-21 08:50:26 -0800

    removes another set of semi colons on bin and passport

--- config/passport.js
+++ config/passport.js
@@ -9,31 +9,31 @@ passport.use(new GoogleStrategy({
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOne({ 'googleId': profile.id }, function(err, user) {
-      if (err) return cb(err);
+      if (err) return cb(err)
       if (user) {
-        return cb(null, user);
+        return cb(null, user)
       } else {
         // we have a new user via OAuth!
         var newUser = new User({
           name: profile.displayName,
           email: profile.emails[0].value,
           googleId: profile.id
-        });
+        })
         newUser.save(function(err) {
-          if (err) return cb(err);
-          return cb(null, newUser);
-        });
+          if (err) return cb(err)
+          return cb(null, newUser)
+        })
       }
-    });
+    })
   }
-));
+))
 
 passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
+    done(null, user.id)
+})
 
 passport.deserializeUser(function(id, done) {
   User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
+    done(err, user)
+  })
+})
