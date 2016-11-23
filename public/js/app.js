ab1f1afc191ccac90d5d1cd340bcfd678b88de47
Darin Ma <darinmma@gmail.com>
2016-11-23 10:45:44 -0800

    adds event listener for search li clicks

--- public/js/app.js
+++ public/js/app.js
@@ -88,8 +88,45 @@ $(document).ready(function(){
     window.location.replace(self.location.href.slice(0,-1) +"/" + searchInput)
   })
 
-  // var ajax = $.get('https://api-public.guidebox.com/v1.43/US/T1srQMKdGpmfuqtp0ciZ7Wfqb82FXc/movies/<%= .id %>')
-  //   .done(function(data){
-  //     console.log(data)
-  //   })
+  function searchApi(req, res, next) {
+  	var searchString = `https://api-public.guidebox.com/v1.43/US/T1srQMKdGpmfuqtp0ciZ7Wfqb82FXc/search/movie/title/${req.params.query}`
+  	  request(searchString, function(err, response, body) {
+  			var results = JSON.parse(body).results
+  			if(results.length >= 5) {
+  				shortArr = []
+  				for(var i = 0; i < 5; i++) {
+  					shortArr.push(results[i])
+  				}
+  				res.render("movies/search", {results: shortArr})
+  			} else {
+  				res.render("movies/search", {results: results})
+  			}
+  			// res.json(JSON.parse(body).results)
+  	  })
+  }
+
+  $('.movieList').click(function(){
+    console.log('clicked')
+    var id = $(this).attr('id')
+    $.ajax({
+    type: 'GET',
+    url: 'https://api-public.guidebox.com/v1.43/US/T1srQMKdGpmfuqtp0ciZ7Wfqb82FXc/movies/' + id
+    }).done(function(data){
+      var title = data.title
+      var image = data.poster_240x342
+      var genre = data.genres[0]
+      var freeSources = data.free_web_sources
+      var subSources =
+      data.subscription_web_sources
+      var purchaseSources =
+      data.purchase_web_sources
+      console.log(data.title)
+      console.log(data.genres[0])
+      console.log(data.poster_240x342)
+    })
+  })
+
+
+
+//end document ready
 })
