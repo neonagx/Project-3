a0a81c9d90f517d21addd3cfc4ccf16ca8894a3f
Gaye Lowenstein <glowen@hotmail.com>
2016-11-27 08:48:10 -0800

    styling to match Paul

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('app.js loaded')
+console.log('app.js loaded');
 
 var searchResults = false
 var title
@@ -9,74 +9,73 @@ var subSources
 var purchaseSources
 
 $(document).ready(function(){
- $(".button-collapse").sideNav()
 
-
-function createMovieHTML(jsonMovie){
-  return $(`<li id="movie-${jsonMovie._id}">Title:${jsonMovie.title}<br>Genre: ${jsonMovie.genre}<br>Provider: ${jsonMovie.providers}<br><img src=${jsonMovie.imageSrc}><input type="checkbox" id=${jsonMovie._id} ${jsonMovie.watched ? "checked" : ""}/><label for=${jsonMovie._id}>Watched</label><br><button class="remove-item">X</span></button></li>`)
-}
-
-$.ajax({
-  type: "GET",
-  url: "/movies/api/movies"
-}).done(function(jsonMovies){
-  jsonMovies.forEach(function(jsonMovie){
-    var movieHTML = createMovieHTML(jsonMovie)
-
-    if(jsonMovie.watched) {
-      $('#watched').append(movieHTML)
-    } else {
-      $('#not-watched').append(movieHTML)
-    }
-  })
-})
-// Attach Listener on form button to perform AJAX post of new movie
-$('#new-movie').on('submit', function(e){
-  //stop the default behavior from clicking on the submit button
-  e.preventDefault()
-
-//Create object representing new movie to be added
-  var newMovie = {
-    title: $('#movie-title').val(),
-    genre: $('#movie-genre').val(),
-    provider: $('#movie-provider').val(),
-    watched: $('#movie-watched').val()
-  }
-  $.post('/movies/api/movies', newMovie).done(function(jsonMovie){
-    //Clear the form
-    $('#movie-title').val('')
-    $('#movie-genre').val('')
-    $('#movie-provider').val('')
-    $('#movie-watched').val('')
-
-    var movieHTML = createMovieHTML(jsonMovie)
-    $('#not-watched').append(movieHTML)
-  })
-})
-
-function updateHandler(e){
-  var html = $(this).parent()
-  var id = html.attr('id').slice(6)
-  console.log(id)
-  $.ajax({
-    type: "PATCH",
-    url: "movies/api/movies/" + encodeURIComponent(id),
-    data: {}
-  }).done(function(jsonMovie){
-    html.remove()
-    var movieHTML = createMovieHTML(jsonMovie)
+  function createMovieHTML(jsonMovie){
     console.log(jsonMovie)
-    if(jsonMovie.watched) {
-      $('#watched').append(movieHTML)
-    } else {
-      $('#not-watched').append(movieHTML)
-    }
-  })
-}
+    return $(`<li id="movie-${jsonMovie._id}">Title: ${jsonMovie.title}<br>Genre: ${jsonMovie.genre}<br><img src=${jsonMovie.imageSrc}><br><input style="color:red" type="checkbox" id=${jsonMovie._id} ${jsonMovie.watched ? "checked" : ""}/><label for=${jsonMovie._id}>Watched</label><br><button class="remove-item">Delete Movie from the list</span></button></li><br>`)
+  }
 
-function deleteHandler(e){
-  var html = $(this).parent()
-  var id = html.attr('id').slice(6)
+  $.ajax({
+    type: "GET",
+    url: "/movies/api/movies"
+  }).done(function(jsonMovies){
+    jsonMovies.forEach(function(jsonMovie){
+      var movieHTML = createMovieHTML(jsonMovie)
+
+      if(jsonMovie.watched) {
+        $('#watched').append(movieHTML)
+      } else {
+        $('#not-watched').append(movieHTML)
+      }
+    })
+  })
+  // Attach Listener on form button to perform AJAX post of new movie
+  $('#new-movie').on('submit', function(e){
+    //stop the default behavior from clicking on the submit button
+    e.preventDefault()
+
+    //Create object representing new movie to be added
+    var newMovie = {
+      title: $('#movie-title').val(),
+      genre: $('#movie-genre').val(),
+      provider: $('#movie-provider').val(),
+      watched: $('#movie-watched').val()
+    }
+    $.post('/movies/api/movies', newMovie).done(function(jsonMovie){
+      //Clear the form
+      $('#movie-title').val('')
+      $('#movie-genre').val('')
+      $('#movie-provider').val('')
+      $('#movie-watched').val('')
+
+      var movieHTML = createMovieHTML(jsonMovie)
+      $('#not-watched').append(movieHTML)
+    })
+  })
+
+  function updateHandler(e){
+    var html = $(this).parent()
+    var id = html.attr('id').slice(6)
+    console.log(id)
+    $.ajax({
+      type: "PATCH",
+      url: "movies/api/movies/" + encodeURIComponent(id),
+      data: {}
+    }).done(function(jsonMovie){
+      html.remove()
+      var movieHTML = createMovieHTML(jsonMovie)
+      console.log(jsonMovie)
+      if(jsonMovie.watched) {
+        $('#watched').append(movieHTML)
+      } else {
+        $('#not-watched').append(movieHTML)
+      }
+    })
+  }
+
+  function deleteHandler(e){
+    var html = $(this).parent()
+    var id = html.attr('id').slice(6)
 
     $.ajax({
       type: "DELETE",
@@ -90,6 +89,11 @@ function deleteHandler(e){
   $('#not-watched').on('click', ':checkbox', updateHandler)
   $('#watched').on('click', '.remove-item', deleteHandler)
   $('#not-watched').on('click', '.remove-item', deleteHandler)
+
+  // $('#submitSearch').click(function(){
+  //   var searchInput = $('#search').val()
+  //   window.location.replace(self.location.href.slice(0,-1) +"/" + searchInput)
+  // })
 
   $('#submitSearch').click(function(){
     $('#searchResults').empty()
@@ -119,45 +123,34 @@ function deleteHandler(e){
       }
     })
   })
-}
 
-$('#watched').on('click', ':checkbox', updateHandler)
-$('#not-watched').on('click', ':checkbox', updateHandler)
-$('#watched').on('click', '.remove-item', deleteHandler)
-$('#not-watched').on('click', '.remove-item', deleteHandler)
-
-$('#submitSearch').click(function(){
-  $('#searchResults').empty()
-  var searchString = $('#search').val()
-  $.ajax({
+  $('#searchResults').on('click', '.clickPic', function(){
+    var id = $(this).prev().attr('id')
+    console.log(id)
+    $.ajax({
     type: 'GET',
-    url: 'https://api-public.guidebox.com/v1.43/US/rKwGrhy6qjpZS1rJFNnX0vpdWMRHMJCo/search/movie/title/' + searchString
-  }).done(function(data){
-    console.log(data)
-    var results = data.results
-    if(results.length >= 5) {
-      shortArr = []
-      for(var i = 0; i < 5; i++) {
-        shortArr.push(results[i])
+    url: 'https://api-public.guidebox.com/v1.43/US/rKwGrhy6qjpZS1rJFNnX0vpdWMRHMJCo/movies/' + id
+    }).done(function(data){
+      var title = data.title
+      var image = data.poster_240x342
+      var genre = data.genres[0].title
+      var freeSources = data.free_web_sources
+      var subSources =
+      data.subscription_web_sources
+      var purchaseSources =
+      data.purchase_web_sources
+      var newMovie = {
+        title: data.title,
+        genre: data.genres[0].title,
+        providers: [data.subscription_web_sources, data.purchase_web_sources],
+        imageSrc: data.poster_240x342
       }
-      shortArr.forEach(function(movie){
-        console.log(movie)
-        $('#searchResults').append(`<div class='searchResponses'><li id='${movie.id}'><a href="">${movie.title}</a></li><img class='clickPic' src="${movie.poster_120x171}">
-        <p> ${movie.rating} | ${ movie.release_date} </p></div>`)
+      $.post('/movies/api/movies', newMovie).done(function(jsonMovie){
+        var movieHTML = createMovieHTML(jsonMovie)
+        $('#not-watched').append(movieHTML)
       })
-    } else {
-      results.forEach(function(movie){
-        console.log(movie)
-        $('#searchResults').append(`<div class='searchResponses'>
-        <li id='${movie.id}'><a>${movie.title}</a></li>
-        <img class='clickPic' src="${movie.poster_120x171}">
-        <p> ${movie.rating} | ${ movie.release_date} </p>
-        </div>`)
-      })
-    }
+    })
   })
-})
-
 
 //end document ready
 })
